671291cacfd09b50b521076f9943f8395e75b6cc
Muskan <103020421+muskan2411@users.noreply.github.com>
2025-07-26 11:39:56 +0530

    Milestone 7 Completed

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import ChatWindow from './components/ChatWindow';
+import { ChatProvider } from './context/ChatContext';
+
+function App() {
+  return (
+    <ChatProvider>
+      <ChatWindow />
+    </ChatProvider>
+  );
+}
+
+export default App;
